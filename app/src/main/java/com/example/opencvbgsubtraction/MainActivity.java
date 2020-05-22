85b6689e681887b94985b87e90eb56ecc23a1374
rishirajrsawant <rishiraj.sawant97@gmail.com>
2020-05-22 03:21:34 +0100

    v.2 trying blob tracking

--- app/src/main/java/com/example/opencvbgsubtraction/MainActivity.java
+++ app/src/main/java/com/example/opencvbgsubtraction/MainActivity.java
@@ -5,13 +5,22 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.SimpleBlobDetector;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.video.*;
 
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -22,7 +31,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private Mat mFGMask;
     private Mat frame;
     private BackgroundSubtractorMOG2 mog2;
-
+    private SimpleBlobDetector simpleBlobDetector;
+    private List<MatOfPoint> contours;
+    private Mat hierarchy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,21 +76,31 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     public void onCameraViewStarted(int width, int height) {
         mRgb = new Mat();
         mFGMask = new Mat();
-        mog2 = Video.createBackgroundSubtractorMOG2();
+        mog2 = Video.createBackgroundSubtractorMOG2(0, 10, true);
+        hierarchy = new Mat();
+        contours = new ArrayList<>();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
-
         frame = inputFrame.rgba();
 
 
         Imgproc.cvtColor(frame, mRgb, Imgproc.COLOR_RGBA2RGB); //the apply function will throw the above error if you don't feed it an RGB image
-        mog2.apply(mRgb, mFGMask); //apply() exports a gray image by definition
+        mog2.apply(mRgb, mFGMask, 0.1); //apply() exports a gray image by definition
         Imgproc.cvtColor(mFGMask, frame, Imgproc.COLOR_GRAY2RGBA);
 
-        //System.gc();
+//        Imgproc.findContours(mFGMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+//        Imgproc.drawContours(frame, contours, -1, new Scalar(100, 125, 230));
+// now iterate over all top level contours
+//        for(int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
+//            MatOfPoint matOfPoint = contours.get(idx);
+//            Rect rect = Imgproc.boundingRect(matOfPoint);
+//            Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(0, 0, 255));
+//
+//        }
+//        //System.gc();
         return frame;
     }
 
